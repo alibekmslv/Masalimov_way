844b3593233461e2dce433f73ae814235b2c4212
Alibek <alibekmslv@gmail.com>
2021-08-02 19:19:47 +0300

    Добавляет сценарии для всплывающих окон
    - Добавляет валидацию для форм.
    - Изменяет разметку форм для валидации.
    - Линт блока modal.scss.
    - Приводит JS-код в единый стиль написания.

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,18 @@
 'use strict';
+
 const KeyCode = {
   LEFT: 37,
   RIGHT: 39,
+  ESCAPE: 27,
 };
 const TAB_SCROLL_OFFSET = 60;
 
 const noJsElement = document.querySelector('.no-js');
 noJsElement.classList.remove('no-js');
 
-const menuButtonElement = document.querySelector('.menu-button');
+// Menu Related Vars
 const mainHeaderElement = document.querySelector('.main-header');
+const menuButtonElement = mainHeaderElement.querySelector('.menu-button');
 
 // Country Cards Related Vars
 const countyCardsElements = document.querySelector('.places-to-visit__country-cards');
@@ -18,35 +21,88 @@ const countyCardsElements = document.querySelector('.places-to-visit__country-ca
 const tabs = document.querySelectorAll('[role="tab"]');
 const tabList = document.querySelector('[role="tablist"]');
 
+// Popup Related Vars
+const modalElement = document.querySelector('.modal');
+const modalCloseElement = modalElement.querySelector('.modal__close');
+const countriesElement = document.querySelector('.countries');
+const pricesElement = document.querySelector('.prices');
+
+// Form Related Vars
+const sectionFeedbackElement = document.querySelector('.section-feedback');
+const feedbackFormElement = sectionFeedbackElement.querySelector('.feedback-form form');
+const feedbackPhoneElement = feedbackFormElement.querySelector('[type="tel"]');
+const feedbackEmailElement = feedbackFormElement.querySelector('[type="email"]');
+const modalResultElement = modalElement.querySelector('.modal__result');
+const buyTourFormElement = modalElement.querySelector('.popup-form form');
+const buyTourPhoneElement = buyTourFormElement.querySelector('[type="tel"]');
+const buyTourEmailElement = buyTourFormElement.querySelector('[type="email"]');
+
+
+// Menu
 menuButtonElement.addEventListener('click', () => {
   mainHeaderElement.classList.toggle('main-header--open');
   menuButtonElement.classList.toggle('menu-button--open');
 });
 
+mainHeaderElement.addEventListener('click', (e) => {
+  if (e.target.classList.contains('site-navigation__link')) {
+    e.preventDefault();
+    mainHeaderElement.classList.remove('main-header--open');
+    menuButtonElement.classList.remove('menu-button--open');
+    document.querySelector(e.target.hash).scrollIntoView({behavior: 'smooth', block: 'start', inline: 'nearest'});
+  }
+});
+
 // Tabs
+const changeTabs = (e) => {
+  const target = e.target;
+  const parent = target.parentNode;
+  const tabsBlock = countriesElement.querySelector('.tabs');
+
+  if (!target.classList.contains('tabs__tab--selected')) {
+    parent.scrollLeft = target.offsetLeft - TAB_SCROLL_OFFSET;
+  }
+
+  parent
+    .querySelectorAll('[aria-selected="true"]')
+    .forEach((t) => {
+      t.setAttribute('aria-selected', false);
+      t.classList.remove('tabs__tab--selected');
+    });
+
+  target.setAttribute('aria-selected', true);
+  target.classList.add('tabs__tab--selected');
+
+  tabsBlock
+    .querySelectorAll('[role="tabpanel"]')
+    .forEach((p) => {
+      p.setAttribute('hidden', true);
+      p.classList.remove('tabs__tabpanel--active');
+    });
+
+  const selectedPanel = tabsBlock.querySelector(`#${target.getAttribute('aria-controls')}`);
+  selectedPanel.removeAttribute('hidden');
+  selectedPanel.classList.add('tabs__tabpanel--active');
+};
+
 if (tabs.length > 0) {
   tabs.forEach((tab) => {
     tab.addEventListener('click', changeTabs);
   });
 
-  // Enable arrow navigation between tabs in the tab list
   let tabFocus = 0;
 
   tabList.addEventListener('keydown', (e) => {
-    // Move down
     if (e.keyCode === KeyCode.RIGHT || e.keyCode === KeyCode.LEFT) {
       e.preventDefault();
       tabs[tabFocus].setAttribute('tabindex', -1);
       if (e.keyCode === KeyCode.RIGHT) {
         tabFocus++;
-        // If we're at the end, go to the start
         if (tabFocus >= tabs.length) {
           tabFocus = 0;
         }
-        // Move up
       } else if (e.keyCode === KeyCode.LEFT) {
         tabFocus--;
-        // If we're at the start, move to the end
         if (tabFocus < 0) {
           tabFocus = tabs.length - 1;
         }
@@ -58,42 +114,6 @@ if (tabs.length > 0) {
   });
 }
 
-function changeTabs(e) {
-  const target = e.target;
-  const parent = target.parentNode;
-  // const grandparent = parent.parentNode;
-  const tabsBlock = document.querySelector('.tabs');
-
-  if (!target.classList.contains('tabs__tab--selected')) {
-    parent.scrollLeft = target.offsetLeft - TAB_SCROLL_OFFSET;
-  }
-
-  // Remove all current selected tabs
-  parent
-    .querySelectorAll('[aria-selected="true"]')
-    .forEach((t) => {
-      t.setAttribute('aria-selected', false);
-      t.classList.remove('tabs__tab--selected');
-    });
-
-  // Set this tab as selected
-  target.setAttribute('aria-selected', true);
-  target.classList.add('tabs__tab--selected');
-
-  // Hide all tab panels
-  tabsBlock
-    .querySelectorAll('[role="tabpanel"]')
-    .forEach((p) => {
-      p.setAttribute('hidden', true);
-      p.classList.remove('tabs__tabpanel--active');
-    });
-
-  // Show the selected panel
-  const selectedPanel = tabsBlock.querySelector(`#${target.getAttribute('aria-controls')}`);
-  selectedPanel.removeAttribute('hidden');
-  selectedPanel.classList.add('tabs__tabpanel--active');
-}
-
 countyCardsElements.addEventListener('click', (e) => {
   e.preventDefault();
   const tabId = e.target.dataset.tab;
@@ -102,7 +122,130 @@ countyCardsElements.addEventListener('click', (e) => {
 
   if (tabId && countryContainerElement) {
     changeTabs({ target: document.querySelector(`#${tabId}`)});
-    // countryContainerElement.scrollIntoView({behavior: 'smooth', block: 'start', inline: 'nearest'});
     tabsElement.scrollIntoView({behavior: 'smooth', block: 'start', inline: 'nearest'});
   }
+});
+
+
+// Popup
+const closeModal = (listenerToRemove) => {
+  modalElement.classList.remove('modal--show');
+  modalResultElement.classList.remove('modal__result--show');
+  window.removeEventListener('keydown', listenerToRemove);
+};
+
+const windowKeydownHandler = (e) => {
+  if (e.keyCode === KeyCode.ESCAPE) {
+    closeModal(windowKeydownHandler);
+  }
+};
+
+const modalCloseClickHandler = () => {
+  closeModal(windowKeydownHandler);
+};
+
+
+const showPopup = () => {
+  modalElement.classList.add('modal--show');
+  window.addEventListener('keydown', windowKeydownHandler);
+};
+
+modalElement.addEventListener('click', (e) => {
+  if (e.target === modalElement) {
+    closeModal(windowKeydownHandler);
+  }
+});
+
+modalCloseElement.addEventListener('click', modalCloseClickHandler);
+
+countriesElement.addEventListener('click', (e) => {
+  if (e.target.classList.contains('country__button')) {
+    e.preventDefault();
+    showPopup();
+  }
+});
+
+pricesElement.addEventListener('click', (e) => {
+  if (e.target.classList.contains('card-price__button')) {
+    e.preventDefault();
+    showPopup();
+  }
+});
+
+
+// Form Validation
+const checkPhoneValidity = (phoneElement) => {
+  const inputElement = phoneElement.parentNode;
+
+  if (phoneElement.validity.patternMismatch) {
+    inputElement.classList.add('input--invalid');
+  } else {
+    inputElement.classList.remove('input--invalid');
+  }
+};
+
+const checkEmailValidity = (emailElement) => {
+  const inputElement = emailElement.parentNode;
+
+  if(!emailElement.validity.valid) {
+    inputElement.classList.add('input--invalid');
+  } else {
+    inputElement.classList.remove('input--invalid');
+  }
+};
+
+// Feedback Form
+const showSuccessPopup = () => {
+  showPopup();
+  localStorage.removeItem('phoneNumber');
+  localStorage.removeItem('email');
+  modalResultElement.classList.add('modal__result--show');
+};
+
+const resetForms = () => {
+  feedbackFormElement.reset();
+  buyTourFormElement.reset();
+};
+
+feedbackPhoneElement.value = localStorage.getItem('phoneNumber');
+feedbackEmailElement.value = localStorage.getItem('email');
+
+feedbackFormElement.addEventListener('submit', (e) => {
+  e.preventDefault();
+  resetForms();
+  showSuccessPopup();
+});
+
+feedbackPhoneElement.addEventListener('input', (e) => {
+  checkPhoneValidity(feedbackPhoneElement);
+  localStorage.setItem('phoneNumber', e.target.value);
+  buyTourPhoneElement.value = e.target.value;
+});
+
+feedbackEmailElement.addEventListener('input', (e) => {
+  checkEmailValidity(feedbackEmailElement);
+  localStorage.setItem('email', e.target.value);
+  buyTourEmailElement.value = e.target.value;
+});
+
+// Buy Tour Form
+buyTourPhoneElement.value = localStorage.getItem('phoneNumber');
+buyTourEmailElement.value = localStorage.getItem('email');
+
+buyTourPhoneElement.addEventListener('input', (e) => {
+  checkPhoneValidity(buyTourPhoneElement);
+  localStorage.setItem('phoneNumber', e.target.value);
+  feedbackPhoneElement.value = e.target.value;
+});
+
+buyTourEmailElement.addEventListener('input', (e) => {
+  checkEmailValidity(buyTourEmailElement);
+  localStorage.setItem('email', e.target.value);
+  feedbackEmailElement.value = e.target.value;
+});
+
+buyTourFormElement.addEventListener('submit', (e) => {
+  e.preventDefault();
+  resetForms();
+  showSuccessPopup();
 });
